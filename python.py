41fb7b2baec053ea4a9ef0b1d19af23ca6bafbe7
Xerby35 <86306783+Xerby35@users.noreply.github.com>
2025-02-20 15:06:19 +0300

    Create python.py

---
+++ python.py
@@ -0,0 +1,24 @@
+"""name=input("What is your name?\n")
+print("Hello"+name)
+sid=int(input("What is your Student ID?\n"))
+print("Your ID is",sid)"""
+
+"""var1=int(input("Enter your first value.\n"))
+var2=int(input("Enter your second value.\n"))
+sum=var1+var2
+diff=var1-var2
+prod=var1*var2
+print(var1,var2,sum,diff,prod)"""
+
+"""name=input("What is your name?\n")
+lab=float(input("What is your lab grade?\n"))
+midterm=float(input("What is your midterm grade?\n"))
+final=float(input("What is your final grade?\n"))
+lastscore=lab*0.25+midterm*0.35+final*0.40
+print("Name: "+name)
+print("Lab: ", lab)
+print("Midterm: ", midterm)
+print("Final: ", final)
+print("Last Score: ", lastscore)"""
+
+#print("*\n""**\n""***\n""**\n""*")
